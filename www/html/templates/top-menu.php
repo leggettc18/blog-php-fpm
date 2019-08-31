c5edeb5313c3939966eb0fae4ffe6e386080d993
Christopher Leggett <chris@leggett.dev>
2019-08-31 14:10:56 -0400

    A few more CSS changes to improve the look of the site

--- www/html/templates/top-menu.php
+++ www/html/templates/top-menu.php
@@ -1,15 +1,15 @@
 <div class="top-menu">
     <div class="menu-options">
         <?php if (isLoggedIn()): ?>
-            <a href="index.php">Home</a>
-            <a href="list-posts.php">All posts</a>
+            <a class="button-primary" href="index.php">Home</a>
+            <a class="button-primary" href="list-posts.php">All posts</a>
             |
-            <a href="edit-post.php">New post</a>
+            <a class="button-primary" href="edit-post.php">New post</a>
             |
             Hello <?php echo htmlEscape(getAuthUser()) ?>
-            <a href="logout.php">Log out</a>
+            <a class="button-primary" href="logout.php">Log out</a>
         <?php else: ?>
-            <a href="login.php">Log in</a>
+            <a class="button-primary" href="login.php">Log in</a>
         <?php endif ?>
     </div>
 </div>
