9fc5594e34a13267acab0de277983eb285417e19
Christopher Leggett <chris@leggett.dev>
2020-01-28 22:26:37 -0500

    Replace another getCommentCount with countByPostId
    Signed-off-by: Christopher Leggett <chris@leggett.dev>

--- www/html/view-post.php
+++ www/html/view-post.php
@@ -12,7 +12,7 @@ if (isset($_GET['post_id']))
 
 // $pdo = getPDO();
 $post = Post::retrieveByPK($postId);
-$commentCount = $post->getCommentCount();
+$commentCount = Comment::countByPostId($post->id);
 
 //If the post does not exist, let's deal with that here
 if (!$post)
