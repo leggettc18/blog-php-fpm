b3ce6afc4c147b1a672ccdfdb9f131fee235c4df
Christopher Leggett <chris@leggett.dev>
2022-04-23 02:18:43 +0000

    Reimplements leggettc18/simple-router to get rid of the "1"

--- public/index.php
+++ public/index.php
@@ -1,13 +1,12 @@
 <?php
 
-use Pecee\SimpleRouter\SimpleRouter;
-
 require '../vendor/autoload.php';
 
 require_once '../src/core/bootstrap.php';
-require_once '../src/routes.php';
+
+use leggettc18\SimpleRouter\{Router, Request};
 
 session_start();
 
-SimpleRouter::start();
-?>
+Router::load('../src/routes.php')
+    ->direct(Request::uri(), Request::method());
--- src/routes.php
+++ src/routes.php
@@ -1,22 +1,19 @@
 <?php
 
-use Pecee\SimpleRouter\SimpleRouter;
-
-SimpleRouter::setDefaultNamespace('\Blog\Controllers');
-
-SimpleRouter::get('/', 'PagesController@home');
-SimpleRouter::get('posts', 'PostController@index');
-SimpleRouter::get('posts/new', 'PostController@new');
-SimpleRouter::post('posts/create', 'PostController@create');
-SimpleRouter::get('posts/show', 'PostController@show');
-SimpleRouter::get('posts/edit', 'PostController@edit');
-SimpleRouter::post('posts/update', 'PostController@update');
-SimpleRouter::post('posts/delete', 'PostController@delete');
-SimpleRouter::post('comments/create', 'CommentController@create');
-SimpleRouter::post('comments/delete', 'CommentController@create');
-SimpleRouter::get('install', 'PagesController@install');
-SimpleRouter::post('install', 'PagesController@install');
-SimpleRouter::get('login', 'PagesController@login');
-SimpleRouter::post('login', 'PagesController@login');
-Simplerouter::get('logout', 'PagesController@logout');
-SimpleRouter::get('xdebug', 'PagesController@xdebug');
+$router->get('', '\Blog\Controllers\PagesController@home');
+$router->get('posts', '\Blog\Controllers\PostController@index');
+$router->get('posts/new', '\Blog\Controllers\PostController@new');
+$router->post('posts/create', '\Blog\Controllers\PostController@create');
+$router->get('assets/main.css', 'assets/main.css');
+$router->get('install', '\Blog\Controllers\PagesController@install');
+$router->post('install', '\Blog\Controllers\PagesController@install');
+$router->get('login', '\Blog\Controllers\PagesController@login');
+$router->post('login', '\Blog\Controllers\PagesController@login');
+$router->get('posts/show', '\Blog\Controllers\PostController@show');
+$router->get('logout', '\Blog\Controllers\PagesController@logout');
+$router->get('posts/edit', '\Blog\Controllers\PostController@edit');
+$router->post('posts/update', '\Blog\Controllers\PostController@update');
+$router->post('posts/delete', '\Blog\Controllers\PostController@delete');
+$router->post('comments/create', '\Blog\Controllers\CommentController@create');
+$router->post('comments/delete', '\Blog\Controllers\CommentController@delete');
+$router->get('xdebug', '\Blog\Controllers\PagesController@xdebug');
